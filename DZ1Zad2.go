09c35b3e56c5a7a8ac8c34115f3688f9f2bbd75a
ILDUS <ildus6696@gmail.com>
2024-03-24 16:11:05 +0300

    Zadanie 2

--- DZ1Zad2.go
+++ DZ1Zad2.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+//Задание 2. Сортировка пузырьком
+//Отсортируйте массив длиной шесть пузырьком.
+
+const size4 = 6
+
+func bubbleSort(input [size4]int) [size4]int {
+	for i := 0; i < len(input)-1; i++ {
+		for j := 0; j < len(input)-i-1; j++ {
+			if input[j] > input[j+1] {
+				input[j], input[j+1] = input[j+1], input[j]
+			}
+		}
+	}
+	return input
+}
+
+func main() {
+	massiv3 := [size4]int{87, 67, 12, 47, 52, 9}
+	fmt.Println(bubbleSort(massiv3))
 }
